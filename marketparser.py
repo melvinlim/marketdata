d8281bca711fa30b0a7000483480f355c67d6577
Melvin Lim <melvinlim@MacBook.local>
2018-05-18 10:04:22 -0700

    add support for quandl api_keys

--- marketparser.py
+++ marketparser.py
@@ -1,5 +1,6 @@
 import urllib
 import re
+from apikey import *
 class MarketParser():
 	def __init__():
 		self.stocks=[]
@@ -11,8 +12,10 @@ class MarketParser():
 		tmp=tmp.strip('\n')
 		tmp=tmp.split('\n')
 		header=self.modifyHeader(tmp[0])
+		self.header=header
 		header=header.split(',')
 		body=tmp[1:]
+		self.body=body
 		for line in body:
 			dLine=dict()
 			tmp=line.split(',')
@@ -40,20 +43,30 @@ class AlphaVantageParser(MarketParser):
 class QuandlParser(MarketParser):
 	def __init__(self,apikey):
 		self.csv=''
-		self.apikey=apikey
+		try:
+			self.apikey=quandl_apikey
+		except:
+			self.apikey=None
 	def modifyHeader(self,header):
 		header=re.sub(r'Date','timestamp',header)
+		header=header.lower()
 		return header
 	def getCSV(self,function,stock):
-		database_code='EOD'
-		database_code='WIKI'
+		if self.apikey:
+			database_code='EOD'
+		else:
+			database_code='WIKI'
 		dataset_code=stock
 		return_format='csv'
 		baseurl='https://www.quandl.com/api/v3/datasets/'+database_code+'/'+dataset_code+'/data.'+return_format
 #		params=urllib.urlencode({'function':function,'symbol':stock,'apikey':self.apikey,'datatype':'csv'})
+		if self.apikey:
+			params=urllib.urlencode({'api_key':self.apikey})
+		else:
+			params=''
 		try:
-#			f=urllib.urlopen(baseurl+'query?%s'%params)
-			f=urllib.urlopen(baseurl)
+			f=urllib.urlopen(baseurl+'?%s'%params)
+#			f=urllib.urlopen(baseurl)
 			self.csv=f.read()
 		except:
 			print f.info()
